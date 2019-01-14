0fafa0e38802dda21eebb1bcd089f94f92c79597
Bruno Santana Alves <bruno.sant.a@gmail.com>
2019-01-14 19:17:52 -0300

    Ajustes nos teste

--- test/lora_test.exs
+++ test/lora_test.exs
@@ -2,7 +2,12 @@ defmodule LoRaTest do
   use ExUnit.Case
   doctest LoRa
 
-  test "greets the world" do
+  test "LoRa Begin" do
     assert LoRa.hello() == :world
+    assert LoRa.start_link() == {:ok, pid}
+    assert LoRa.begin(433.0e6) == :ok
+    assert LoRa.set_spreading_factor(10) == :ok
+    assert LoRa.set_signal_band_width(62.5e3) == :ok
+    assert LoRa.enable_crc() == :ok
   end
 end
