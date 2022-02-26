e00268da5150d8b4e45583572cbe779a993b0c93
Rockwell Schrock <rockwell@schrock.me>
2022-02-25 19:40:28 -0500

    Add three encoding schemes for packet data

--- lib/lora.ex
+++ lib/lora.ex
@@ -1,7 +1,7 @@
 defmodule LoRa do
   @moduledoc """
   This is a module for transmitter data using LoRa Radios.
-  
+
   Radios:
       Semtech SX1276/77/78/79 based boards.
   """
@@ -13,6 +13,7 @@ defmodule LoRa do
 
   alias LoRa.Modem
   alias LoRa.Communicator
+  alias LoRa.Encoding
 
   require Logger
 
@@ -20,36 +21,41 @@ defmodule LoRa do
   @lora_default_spi "spidev0.0"
   @lora_default_spi_frequency 8_000_000
   @lora_default_reset_pin 25
+  @lora_default_encoding :inspect
   # @lora_default_dio0_pin 22
-  
+
   @doc """
   Start and link a new GenServer for LoRa radio.
 
   Can be set the parameters of SPI and GPIO pin for the reset of radio.
-  
+
     # standard values: `spi: "spidev0.0", spi_speed: 8_000_000, rst: 25`
       {:ok, lora} = LoRa.start_link()
     or
       {:ok, lora} = LoRa.start_link([spi: "spidev0.1", spi_speed: 5_000_000, rst: 27])
-  
+
   """
   def start_link(config \\ []), do: GenServer.start_link(__MODULE__, config ++ [owner: self()])
+
   @doc """
   Initialize the LoRa Radio and set your frequency work.
       {:ok, lora} = LoRa.start_link()
       LoRa.begin(lora, 433.0e6)
   """
   def begin(pid, frequency), do: GenServer.call(pid, {:begin, frequency})
+
   @doc """
-  Set the LoRa Radio in sleep mode. 
+  Set the LoRa Radio in sleep mode.
       LoRa.sleep(lora_pid)
   """
   def sleep(pid), do: GenServer.cast(pid, :sleep)
+
   @doc """
   Awake the LoRa Radio.
       LoRa.awake(lora_pid)
   """
   def awake(pid), do: GenServer.cast(pid, :awake)
+
   @doc """
   Set Spreading Factor. `sf` is a value between 6 and 12. Standar value is 6.
 
@@ -57,24 +63,28 @@ defmodule LoRa do
   """
   def set_spreading_factor(pid, sf \\ 6) when sf >= 6 and sf <= 12,
     do: GenServer.cast(pid, {:set_sf, sf})
+
   @doc """
   Set Trasmission Signal Bandwidth.
 
       LoRa.set_signal_bandwidth(lora_pid, 31.25e3)
   """
   def set_signal_bandwidth(pid, sbw), do: GenServer.cast(pid, {:set_sbw, sbw})
+
   @doc """
   This is a verify digit add in the data message.
 
       LoRa.enable_crc(lora_pid)
   """
   def enable_crc(pid), do: GenServer.cast(pid, :enable_crc)
+
   @doc """
   Remove the verify digit.
 
       LoRa.disable_crc(lora_pid)
   """
   def disable_crc(pid), do: GenServer.cast(pid, :disable_crc)
+
   @doc """
   Send data for other radios.
 
@@ -91,10 +101,18 @@ defmodule LoRa do
     pin_reset = Keyword.get(config, :rst, @lora_default_reset_pin)
     device = Keyword.get(config, :spi, @lora_default_spi)
     speed_hz = Keyword.get(config, :spi_speed, @lora_default_spi_frequency)
+    encoding = Keyword.get(config, :encoding, @lora_default_encoding)
 
     {:ok, spi} = start_spi(device, speed_hz)
 
-    state = %{is_receiver?: true, owner: config[:owner], spi: nil, rst: nil, config: nil}
+    state = %{
+      is_receiver?: true,
+      owner: config[:owner],
+      encoding: encoding,
+      spi: nil,
+      rst: nil,
+      config: nil
+    }
 
     Logger.info("LoRa: Start Device")
     {:ok, rst} = GPIO.start_link(pin_reset, :output)
@@ -120,7 +138,7 @@ defmodule LoRa do
 
   def handle_info({:receive_msg, pkt_length}, state) do
     Logger.debug("LoRa: message recieved: #{pkt_length}Bytes")
-    Modem.read(state.config.frequency, state.owner, state.spi)
+    Modem.read(state.config.frequency, state.owner, state.spi, state.encoding)
     {:noreply, state}
   end
 
@@ -166,9 +184,11 @@ defmodule LoRa do
     Kernel.send(self(), :receiver_mode)
     Modem.idle(state.spi)
     GenServer.cast(__MODULE__, {:header_mode, header})
+
     data
-    |> Kernel.inspect()
+    |> Encoding.encode_data!(state.encoding)
     |> Communicator.print(state.spi)
+
     self() |> Modem.end_packet(state.spi)
 
     {:noreply, %{state | is_receiver?: true}}
